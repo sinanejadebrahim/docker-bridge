2493e2d52bdc9fdfe57a06298c963780c6b64296
Sina Nejadebrahim <sina.n@smartech.ir>
2022-11-19 02:55:23 +0330

    Added xz compression

--- client.sh
+++ client.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 echo "    made by: "
 echo -e "\e[0;35m 😈 SINA NEJADEBRAHIM 😈 \e[0m \n\n"
-echo -e "working . . ."
+echo "Compressing & Pulling the image to make it smaller for download, this might take some time"
+for i in {1..3};do
+  echo 'Compressing...'
+  sleep 1
+done
+echo "Compression made the image 5 times smaller, Enjoy (: "
 {
 IMG=$1
-RES=$(curl -s http://IP:PORT/hooks/pull?img=$IMG)
-
+TIMG="$(echo $IMG | sed 's/\:/-/' | cut -f2 -d/).tar"
+RES=$(curl -s http://IP:54321/hooks/pull?img=$IMG)
 while [ -z "$RES" ];
 do
-  sleep 5
+  sleep 3
 done
 } &> /dev/null
 echo -e "\e[0;32m"
 echo "Downloading image . . ."
-wget $(echo $RES | awk '{print $2}') -O /tmp/$IMG.tar -q --show-progress
+wget $(echo $RES | awk '{print $2}') -O /tmp/$TIMG.xz -q --show-progress
 echo -e "\e[0m"
 {
-curl -s http://IP:PORT/hooks/stop-server > /dev/null 2>&1
-docker load -i /tmp/$IMG.tar
+curl -s http://IP:54321/hooks/stop-server > /dev/null 2>&1
+tar axf /tmp/$TIMG.xz 
+docker load -i $TIMG
 } &> /dev/null
 
 echo "your image is available now"
-docker image ls | grep $IMG
-rm /tmp/$IMG.tar 
+docker image ls | grep $(echo $IMG | cut -f1 -d:)
+rm /tmp/$TIMG.xz
--- server/run.sh
+++ server/run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 IMG=$1
-
+IP=$2
+TIMG="$(echo $IMG | sed 's/\:/-/' | cut -f2 -d/).tar"
 /bin/bash /root/imager/start-server.sh > /dev/null 2>&1
 
 docker pull $IMG >/dev/null 2>&1
-docker save $IMG -o /root/images/$IMG.tar
 
-echo "URL: IP:PORT/$IMG.tar"
+docker save $IMG -o /root/images/$TIMG
+cd /root/images
+tar acf "$TIMG.xz" "$TIMG"
+rm $TIMG
+
+echo "URL: IP:5432/$TIMG.xz"
